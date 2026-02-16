5b581491d53457b77fab4a4659eca0a1e393b43b
unknown <beforeyouknowkindness@gmail.com>
2026-02-15 18:47:19 -0600

    update test queries and data

--- query.js
+++ query.js
@@ -5,24 +5,44 @@ const path = require('path');
 const os = require('os')
 const { queryApi } = require('sec-api');
 // =======================================QUERIES=======================================
-const testCusip = ""
+// const testCusip = ""
+const testCIK = "0000928047 1046192 1094584"
+const testCIKArr = testCIK.split(" ")
+const cikArr = []
 
+for (let i = 0; i < testCIKArr.length; i++) {
+    cikArr.push(testCIKArr[i].toString().replace(/^0+/, ''))
+
+}
+
+
+let queryStr = `cik:(${testCIK})`
+
+// "formType:13F AND NOT formType:NT AND NOT formType:A AND periodOfReport:[2025-01-01 TO 2025-12-31] AND (928047, 320193, 1065280)"
+// formType:13F AND NOT formType:NT AND NOT formType:A AND periodOfReport:[2025-01-01 TO 2025-12-31] AND (cik:928047 OR cik:320193 OR cik:1065280)
+// formType:13F AND NOT formType:NT AND NOT formType:A AND periodOfReport:[2025-01-01 TO 2025-12-31] AND (${queryStr})
 
 // Query to grab form 13F-HR for a specified cik
-// const query = {
+// const queryObj = {
 //     query: `formType:13F AND cik:${cik} AND NOT formType:NT AND NOT formType:A`,
 //     from: '0', // start with first filing. used for pagination/skipping entries
 //     size: '4', // limit response to # of filings, max 50
 //     sort: [{ filedAt: { order: 'desc' } }], // sort result by filedAt, newest first
 // }
 
-
-const query = {
-    query: `formType:13F AND holdings.cusip:${testCusip}`,
-    from: "0",
-    size: "50",
-    sort: [{ filedAt: { order: 'desc' } }]
+const queryObj = {
+    query: `(formType:13F AND NOT formType:NT AND NOT formType:A AND periodOfReport:[2025-01-01 TO 2025-12-31]) AND (cik:928047 OR cik:1094584 OR cik:1046192)`,
+    from: '0', // start with first filing. used for pagination/skipping entries
+    size: '50', // limit response to # of filings, max 50
+    sort: [{ filedAt: { order: 'desc' } }], // sort result by cik
 }
+
+// const queryObj = {
+//     query: `formType:13F AND holdings.cusip:${testCusip}`,
+//     from: "0",
+//     size: "50",
+//     sort: [{ filedAt: { order: 'desc' } }]
+// }
 
 
 // =======================================MAIN VARIABLES=======================================
@@ -49,16 +69,19 @@ function processArgs() {
 }
 
 // Makes api request; returns json result
-async function query(cik) {
+async function query(queryObj) {
     // Trims leading 0s from CIK for query formatting
     cik = cik.toString().replace(/^0+/, '');
     return await queryApi.getFilings(query);
 }
 
 
-function main() {
+async function main() {
     // for (const cik of cikArray) {
-    fs.writeFile("./query_output.json", JSON.stringify(query(cik)), err => {
+
+    let results = await queryApi.getFilings(queryObj)
+
+    fs.writeFile("./query_output.json", JSON.stringify(results), err => {
         if (err) {
             console.error(err);
         } else {
@@ -67,3 +90,4 @@ function main() {
     });
     // }
 }
+main()
